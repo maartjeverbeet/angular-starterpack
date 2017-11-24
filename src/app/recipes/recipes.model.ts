0c35bb155ab499bea9979c161dbf11d39d3a8f67
Marc den Uil <mcuil@avans.nl>
2017-11-24 18:41:42 +0100

    Trying to fix problem with displaying detail recipe

--- src/app/recipes/recipes.model.ts
+++ src/app/recipes/recipes.model.ts
@@ -1,13 +1,15 @@
 import { Ingredient } from '../shared/ingredient.model';
 
 export class Recipe {
+  public _id: number;
   public name: string;
   public description: string;
   public imagePath: string;
   public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
+  constructor(name: string, _id: number, desc: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
+    this._id = _id;
     this.description = desc;
     this.imagePath = imagePath;
     this.ingredients = ingredients;
