f7aa57934dccb590a15dc87532e39bd3b08d793c
Marc den Uil <mcuil@avans.nl>
2017-11-27 15:40:00 +0100

    Removing unnecessary things

--- src/app/recipes/recipes.model.ts
+++ src/app/recipes/recipes.model.ts
@@ -7,47 +7,4 @@ export class Recipe {
   private _imagePath: string;
   private _ingredients: Ingredient[];
 
-  constructor(values: Object = {}) {
-    Object.assign(this, values);
-  }
-
-  public get _id(): string {
-    return this.id;
-  }
-
-  public set _id(n: string) {
-    this.id = n;
-  }
-
-  public get name(): string {
-    return this._name;
-  }
-
-  public set name (n: string) {
-    this._name = n;
-  }
-
-  public get description(): string {
-    return this._description;
-  }
-
-  public set description(d: string) {
-    this._description = d;
-  }
-
-  public get imagePath(): string {
-    return this._imagePath;
-  }
-
-  public set imagePath(i: string) {
-    this._imagePath = i;
-  }
-
-  public get ingredients(): Ingredient[] {
-    return this._ingredients;
-  }
-
-  public set ingredients(i: Ingredient[]) {
-    this._ingredients = i;
-  }
 }
