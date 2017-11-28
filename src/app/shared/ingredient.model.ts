703ebd18611fabb5b531d98cd7b221cff1fd9b5d
Imottall <tjbuiten@avans.nl>
2017-11-28 15:31:08 +0100

    Hopefully fixed getting the ingredients.... again.... and again..... and again..... and again? Man I love my job!..... I'm done... Does someone have pizza?... and again... added logs.. updated logs.. changed id thingy.. again... am I in a while(true) loop?... still stuck... yay.. fml

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,33 +1,3 @@
 export class Ingredient {
-  public _name: string;
-  public _amount: number;
-  public id: string;
-
-  constructor(values: Object = {}) {
-    Object.assign(this, values);
-  }
-
-  public get _id(): string {
-    return this.id;
-  }
-
-  public set _id(n: string) {
-    this.id = n;
-  }
-
-  public get name(): string {
-    return this._name;
-  }
-
-  public set name (n: string) {
-    this._name = n;
-  }
-
-  public get amount(): number {
-    return this._amount;
-  }
-
-  public set amount(d: number) {
-    this._amount = d;
-  }
+  constructor(public name: string, public amount: number, public _id: string) {}
 }
