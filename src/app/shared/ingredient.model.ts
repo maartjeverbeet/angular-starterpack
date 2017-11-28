79f8e3718c60c79fbfae37e72f9839e2f4a6a837
Imottall <tjbuiten@avans.nl>
2017-11-28 13:55:36 +0100

    Hopefully fixed getting the ingredients.... again.... and again..... and again.....

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,3 +1,33 @@
 export class Ingredient {
-  constructor(public name: string, public amount: number, public _id: number) {}
+  public _name: string;
+  public _amount: number;
+  public id: string;
+
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
+
+  public get _id(): string {
+    return this.id;
+  }
+
+  public set _id(n: string) {
+    this.id = n;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name (n: string) {
+    this._name = n;
+  }
+
+  public get amount(): number {
+    return this._amount;
+  }
+
+  public set amount(d: number) {
+    this._amount = d;
+  }
 }
